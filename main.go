4c6d3f6fe5dc8ad7d854ab3ffb0422ac40195f2e
ge1s7 <kacperkalinowski@protonmail.com>
2025-06-06 18:00:44 +0200

    b ADD review/reset metrics

--- main.go
+++ main.go
@@ -12,8 +12,12 @@ type apiConfig struct {
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	_ = cfg.fileserverHits.Add(1)
-	return next
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		fmt.Println("add 1")
+		next.ServeHTTP(w, r)
+	})
+
 }
 
 func healthHandler(w http.ResponseWriter, req *http.Request) {
@@ -21,20 +25,17 @@ func healthHandler(w http.ResponseWriter, req *http.Request) {
 	header := w.Header()
 	header.Set("Content-Type", "text/plain; charset=utf-8")
 
-	msg := []byte("OK")
-
 	w.WriteHeader(http.StatusOK)
+	msg := []byte("OK")
 	w.Write(msg)
 }
 
 func (cfg *apiConfig) reqNumHandler(w http.ResponseWriter, req *http.Request) {
-
 	header := w.Header()
 	header.Set("Content-Type", "text/plain; charset=utf-8")
 
-	msg := []byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load()))
-
 	w.WriteHeader(http.StatusOK)
+	msg := []byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load()))
 	w.Write(msg)
 
 }
@@ -44,29 +45,28 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
 	header := w.Header()
 	header.Set("Content-Type", "text/plain; charset=utf-8")
 
-	cfg.fileserverHits.Store(0)
-
-	msg := []byte(fmt.Sprintf("fileserverHits has been reset to 0"))
+	cfg.fileserverHits.Swap(0)
 
 	w.WriteHeader(http.StatusOK)
+	msg := []byte(fmt.Sprintf("fileserverHits has been reset to 0"))
 	w.Write(msg)
 }
 
 func main() {
 	cfg := apiConfig{}
+
 	mux := http.NewServeMux()
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	mux.HandleFunc("/healthz", healthHandler)
-	mux.HandleFunc("/metrics", cfg.reqNumHandler)
-	mux.HandleFunc("/reset", cfg.resetHandler)
-
 	fsh := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
-
 	mux.Handle("/app/", cfg.middlewareMetricsInc(fsh))
+
+	mux.HandleFunc("/healthz/", healthHandler)
+	mux.HandleFunc("/metrics/", cfg.reqNumHandler)
+	mux.HandleFunc("/reset/", cfg.resetHandler)
 
 	log.Fatal(server.ListenAndServe())
 
