1cb5b87da17bd77b8408be6fe9e23133e84c215c
ge1s7 <kacperkalinowski@protonmail.com>
2025-06-08 16:39:11 +0200

    add CheckPasswordHash

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -18,5 +18,10 @@ func HashPassword(password string) (string, error) {
 }
 
 func CheckPasswordHash(hash, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return err
+	}
+	return nil
 
 }
