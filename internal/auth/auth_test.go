867bfd2db6519bf8441101abaaedc9ec385241f0
ge1s7 <kacperkalinowski@protonmail.com>
2025-06-09 17:57:13 +0200

    ADD test invalid pass hash pairs

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,8 +1,10 @@
 package auth
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestHashPassword(t *testing.T) {
+func TestCreatedFunctions(t *testing.T) {
 	var passHash = make(map[string]string)
 	passwords := []string{"test", "asdfasd", "%6je134", "aPldfjnme$3", "!?@klQiQ56.-"}
 
@@ -23,4 +25,28 @@ func TestHashPassword(t *testing.T) {
 	}
 }
 
-// func TestCheckPasswordHash(t *testing.T) {}
+func TestInvalidPairs(t *testing.T) {
+	wrongPassHash := map[string]string{
+		"test": "jj9",
+		"dsae": "8989898989dsf",
+		"888":  "aaAAasa8888888",
+	}
+
+	for key, value := range wrongPassHash {
+		err := CheckPasswordHash(value, key)
+		if err == nil {
+			t.Errorf("False positive hash check: %v", err)
+		}
+	}
+}
+
+//func TestHashPassword(t *testing.T) {
+//	passHashBcrypt := make(map[[]byte][]byte)
+//	passwords := []string{"test", "asdfasd", "%6je134", "aPldfjnme$3", "!?@klQiQ56.-"}
+//	for _, e := range passwords {
+//		hashed, err := bcrypt.GenerateFromPassword(byte(e))
+//		if err != nil {
+//			t.Errorf("Bcrypt hashing error: %v", err)
+//		}
+//	}
+//}
